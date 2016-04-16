5b7868528ec3b14c50304ac49c3c55315be41fa4
MacChris <macchris@10.0.2.15>
2016-04-16 02:35:54 -0700

    completed Part-3

--- FCTwenteNieuws/APIManager.swift
+++ FCTwenteNieuws/APIManager.swift
@@ -18,29 +18,47 @@ class APIManager {
         
         //let session = NSURLSession.sharedSession()
         let url = NSURL(string: urlString)!
-        
+
         let task = session.dataTaskWithURL(url) {
-            (data, response, error) -> Void in
+            (data,response, error) -> Void in
             
-            dispatch_async(dispatch_get_main_queue()) {
-                if error != nil {
+            if error != nil {
+                dispatch_async(dispatch_get_main_queue()) {
                     completion(result: (error!.localizedDescription))
-                    
-                 
-                } else {
-                    completion(result: "NSURLSession succesful")
-                    print(data)
                 }
+            } else {
+                
+                //added for JSONSerialization
+                //print(data)
+                do {
+                    
+                    if let json = try NSJSONSerialization.JSONObjectWithData(data!,
+                    options: .AllowFragments)
+                        as? [String: AnyObject] {
+                            
+                            print(json)
+                            
+                            let priority = DISPATCH_QUEUE_PRIORITY_HIGH
+                            dispatch_async(dispatch_get_global_queue (priority, 0)) {
+                              dispatch_async(dispatch_get_main_queue()) {
+                                completion(result: "JSONSerialization was een succes!")
+                            }
+                    }
+                }
+            } catch {
+                dispatch_async(dispatch_get_main_queue()) {
+                    completion(result: "error in JSONSerilazation")
+                    
+                }
+                
+              }
+   
             }
-        }
-       
-      task.resume()
-        
-    }
-    
+}
+task.resume()
+}
 }
 
-    
-    
-    
+
+
 
